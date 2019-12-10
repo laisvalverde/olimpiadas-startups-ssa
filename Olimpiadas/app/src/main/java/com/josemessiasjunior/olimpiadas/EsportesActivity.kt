a0db2c7034ed05e058cd7931b4008e9f6e617fae
jose <jrsantos18@gmail.com>
2019-12-10 20:32:32 -0300

    tela de escolher esporte

--- Olimpiadas/app/src/main/java/com/josemessiasjunior/olimpiadas/EsportesActivity.kt
+++ Olimpiadas/app/src/main/java/com/josemessiasjunior/olimpiadas/EsportesActivity.kt
@@ -8,5 +8,7 @@ class EsportesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_esportes)
+
+        val nomeDoTime1 = intent.getStringExtra("TIME1")
     }
 }
